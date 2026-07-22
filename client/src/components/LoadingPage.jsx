28148975d03e5a3b444044cb6bd088e6f630087b
dylan <dkooby1@gmail.com>
2026-07-22 18:49:07 -0400

    More changes to notifications socket handler

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -1,9 +1,8 @@
-import Navbar from "./Navbar.jsx";
 import {FourSquare} from "react-loading-indicators";
 
 export default function LoadingPage() {
     return (
-        <div className={`flex flex-col w-screen h-screen bg-stone-50 dark:bg-zinc-900`}>
+        <div className={`flex flex-col w-full h-full bg-stone-50 dark:bg-zinc-900`}>
             <div className={`m-auto`}>
                 <FourSquare color="#27272a" size="medium" text="" textColor="" />
             </div>
